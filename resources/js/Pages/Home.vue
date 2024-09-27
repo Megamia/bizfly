b0f4034acc927f9914f8413787d15b43039de5d8
Megamia <luudat214@gmaill.com>
2024-09-27 11:00:09 +0700

    update

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>Home</div>
+    <div>
+        <a-row
+            style=" height: 64px"
+            align="center"
+                justify="center"
+        >
+            <a-col
+                :span="6"
+                style="height: 100%"
+                
+                ><HomeOutlined
+            /></a-col>
+            <a-col :span="6">col-6</a-col>
+            <a-col :span="6">col-6</a-col>
+            <a-col :span="6">col-6</a-col>
+        </a-row>
+    </div>
 </template>
 
-<script setup></script>
+<script lang="ts" setup>
+import { HomeOutlined } from "@ant-design/icons-vue";
+</script>
 
 <style lang="css"></style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,9 @@ import App from "./App.vue";
 import "./bootstrap";
 import router from "./Router.";
 import { createApp } from "vue";
+import "ant-design-vue/dist/reset.css";
+import { Row, Col } from "ant-design-vue";
 
 const app = createApp(App);
 
-app.use(router).mount("#app");
+app.use(router).use(Row).use(Col).mount("#app");
